2a959230b1f3281953b69c8189b1bdee0aaa4534
austintedwards <austintedwards@gmail.com>
2017-10-15 11:30:54 -0600

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
+
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,25 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+
+
+     ionViewDidEnter() {
+
+       /*Initializing Map*/
+      const ACCESS_TOKEN = 'pk.eyJ1IjoiYXVzdGtlIiwiYSI6ImNpd21uZTB1bDAwNm8yenF4ZmtlbjkzenUifQ.CohFKxWoYGrFXQDoRvZWag';
+       mapboxgl.accessToken = ACCESS_TOKEN;
+       var map = new mapboxgl.Map({
+      style: 'mapbox://styles/mapbox/light-v9',
+      center: [-74.0066, 40.7135],
+      zoom: 16,
+      // pitch: 80,
+      minZoom: 7.5, //restrict map zoom - buildings not visible beyond 13
+      maxZoom: 17,
+      container: 'map'
+    });
+
+  }
+
 
 }
